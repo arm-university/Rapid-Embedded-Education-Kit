6922625ec951f2bdd88a15bb2009fa91bba2db66
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2022-07-08 12:32:52 +0100

    Fixed jingle bells
    Ears not objecting quite as much now :)

--- Tasks/module8-4-5-JingleBells/main.cpp
+++ Tasks/module8-4-5-JingleBells/main.cpp
@@ -12,6 +12,7 @@ Output: PWM Speaker (play the music), and RGB LED (reflect the melody)
 #include "mbed.h"
 #include <chrono>
 #include <ratio>
+#include <math.h>
 
 #define BUTTON_1 D2
 #define BUTTON_2 D3
@@ -22,25 +23,36 @@ Output: PWM Speaker (play the music), and RGB LED (reflect the melody)
 #define BLUE_LED D8
 #define SPEAKER D9
 
-//Define the frequency of basic music notes
-# define Do     0.5
-# define Re     0.45
-# define Mi     0.4
-# define Fa     0.36
-# define So     0.33
-# define La     0.31
-# define Si     0.3
-# define No     0 
+// Frequency of middle C (Hz)
+#define MIDDLEC 261.63
+double noteFreq[12];    //C,C#,D,D#,E,F,F#,G,G#,A,A#,B
+double notePeriod[12];
+void preCalculateNotes()
+{
+    for (unsigned n=0; n<12; n++) {
+        noteFreq[n] = MIDDLEC*pow(2.0, (double)n/12.0); // f_n = root * 2^(n/12)
+        notePeriod[n] = (1.0 / noteFreq[n]);              // T_n = 1/T_n
+    }
+}
+
+//Define the frequency of basic music notes (no accidentals)
+#define Do(octave)  ((double)octave*noteFreq[0])      //C
+#define Re(octave)  ((double)octave*noteFreq[2])      //D        
+#define Mi(octave)  ((double)octave*noteFreq[4])      //E
+#define Fa(octave)  ((double)octave*noteFreq[5])      //F
+#define So(octave)  ((double)octave*noteFreq[7])      //G
+#define La(octave)  ((double)octave*noteFreq[9])      //A
+#define Si(octave)  ((double)octave*noteFreq[11])     //B
+#define No          0.0                         //PAUSE - never set the PWM period to 0
 
 //Define the beat length (e.g. whole note, half note) 
-# define b1     0.5
-# define b2     0.25
-# define b3     0.125
-# define b4     0.075
+#define b1     0.5
+#define b2     0.25
+#define b3     0.125
+#define b4     0.075
 
-//Define the musical piece
-float note[] = {Mi,No,Mi,No,Mi,No, Mi,No,Mi,No,Mi,No, Mi,No,So,No,Do,No,Re,No,Mi,No, Fa,No,Fa,No,Fa,No,Fa,No, Fa,No,Mi,No,Mi,No,Mi,No,Mi,No, Mi,Re,No,Re,Mi, Re,No,So,No};
-float beat[] = {b3,b3,b3,b3,b2,b2, b3,b3,b3,b3,b2,b2, b3,b3,b3,b3,b3,b3,b3,b3,b2,b1, b3,b3,b3,b3,b3,b3,b3,b3, b3,b3,b3,b3,b3,b3,b4,b4,b4,b4, b2,b3,b3,b2,b2, b2,b2,b2,b2};
+//Blue button to start
+DigitalIn blueButton(BUTTON1);
 
 //Define the analog inputs
 DigitalIn volume(BUTTON_1);
@@ -54,56 +66,48 @@ PwmOut RedLed(RED_LED);
 PwmOut GreenLed(GREEN_LED);
 PwmOut BlueLed(BLUE_LED);
 
-DigitalIn test (D7);
-
-//Define the time ticker
-Ticker timer; 
-
-//Static variable
-static int k;
-
-
-/*----------------------------------------------------------------------------
- Interrupt Service Routine
- *----------------------------------------------------------------------------*/
-
-//Define the ticker ISR
-void timer_ISR(){ 
-
-
-	if (k<(sizeof(note)/sizeof(int))){
-
-		if(note[k] == No)                       		//If there is a sound pause
-			Speaker = 0;                          		//Set the PWM duty cycle to zero
-		else {
-			Speaker.period(0.005*note[k]);   			//Set the PWM period, which determines the note of the sound
-            if (volume) Speaker = 0.8;          		//Set the PWM duty cycle that determines the volume of the sound (controlled by one potentiometer)
-            else Speaker = 0.2;
-        }
-
-		k++;
-			
-		//Set the time for the next ticker interrupt, this is determined by the default music beat and the potentiometer
-        float timeIn_ms = 1000.0f*((beat[k]/2)+0.5);
-		timer.attach(&timer_ISR, chrono::milliseconds( (uint32_t)timeIn_ms ));
-			
-		//RGB LED indicator
-    	RedLed = note[k];
-    	GreenLed = beat[k];
-    	BlueLed = Speaker;
-	}else{
-		k = 0;                                 			//If the musical piece is finished, start again
-		Speaker = 0;
-	}
-} 
 
 /*----------------------------------------------------------------------------
  MAIN function
  *----------------------------------------------------------------------------*/
 
+using namespace std;
 int main(){
-	timer.attach(&timer_ISR, 100ms);    					//Initialize the time ticker
-    k=0;
+    //Initialise the arrays
+    preCalculateNotes();
+
+    //Define the musical piece
+    double note[] = { Mi(1),No,Mi(1),No,Mi(1),No, 
+                      Mi(1),No,Mi(1),No,Mi(1),No,
+                      Mi(1),No,So(1),No,Do(1),No,Re(1),No,Mi(1),No, 
+                      Fa(1),No,Fa(1),No,Fa(1),No,Fa(1),No,Fa(1),No,Mi(1),No,Mi(1),No,Mi(1),No,Mi(1),No,Mi(1),Re(1),No,Re(1),Mi(1), Re(1),No,So(1),No};
+
+    double beat[] = { b3,b3, b3,b3, b2,b2, 
+                      b3,b3, b3,b3, b2,b2, 
+                      b3,b3, b3,b3, b3,b3, b3,b3, b2,b1, b3,b3, b3,b3, b3,b3, b3,b3, b3,b3, b3,b3, b3,b3, b4,b4, b4,b4, b2,b3, b3,b2, b2, b2,b2, b2,b2};
+
+    //Wait for button press
+    while (blueButton == 1);
+
+    //Play tune
+    for (unsigned int n=0; n< sizeof(note)/sizeof(double); n++) {
+        double nextNote     = note[n];       //This is the period of the note
+        double nextDuration = beat[n];   //This is in seconds
+        printf("note: %10.8f\tduration: %4.1f\n\r", nextNote, nextDuration);
+
+        if (nextNote == No) {
+            //Rest
+            Speaker = 0.0;
+        } else {
+            //Tone
+            double T = 1.0/nextNote;     //Period of the next note
+            Speaker.period(T);
+            Speaker = 0.5;
+        }
+        
+        // Pause to allow the note or rest to complete 
+        wait_us(beat[n]*1000000);
+    }
 
     while(1){ 
 		__WFI();
